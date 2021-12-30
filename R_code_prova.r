202068e93d7892a6a0b88f37872a6b0bc40918a4
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2021-12-30 23:55:15 +0100

    Update R_code_prova.r

--- R_code_prova.r
+++ R_code_prova.r
@@ -2,8 +2,9 @@
 
 setwd("/Users/federicotossani/Desktop/R_datacamp/bird_pop_08-12/Art12_MS_2015_csv/")
 require(ggplot2)
+require(dplyr)      #install.packages('dplyr')
 
-bird <- read.csv("data_birds.csv", stringsAsFactors = FALSE)
-str(bird)
-
-/Users/federicotossani/Desktop/R_datacamp/bird_pop_08-12/Art12_MS_2015_csv/
+birds <- read.csv("data_birds.csv", stringsAsFactors = FALSE)
+birds <- as_tibble(birds)
+#str(birds)
+birds <- select(birds, "country", "speciesname", "population_maximum_size")
