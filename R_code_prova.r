aaad0929b1eb9f7136d993c1e9a309fa3c8c4dc7
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2021-12-30 23:10:39 +0100

    Update R_code_prova.r

--- R_code_prova.r
+++ R_code_prova.r
@@ -1,1 +1,9 @@
 ### Uccelli ###
+
+setwd("/Users/federicotossani/Desktop/R_datacamp/bird_pop_08-12/Art12_MS_2015_csv/")
+require(ggplot2)
+
+bird <- read.csv("data_birds.csv", stringsAsFactors = FALSE)
+str(bird)
+
+/Users/federicotossani/Desktop/R_datacamp/bird_pop_08-12/Art12_MS_2015_csv/
